d722a2f7b308b28831489582710edd0771c468f6
Sultana Razia <sultanarazia483@gmail.com>
2024-12-17 09:50:52 +0600

    Basic project setup and server connect with mongodb

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+
+const corsOption = {
+    origin: ['http://localhost:5000/', 'http://localhost:5173/'],
+    Credential: true,
+    optionSuccessStatus: 200,
+}
+
+//middleware
+app.use(cors(corsOption));
+app.use(express.json());
+
+
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.qtkz8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.qtkz8.mongodb.net:27017,cluster0-shard-00-01.qtkz8.mongodb.net:27017,cluster0-shard-00-02.qtkz8.mongodb.net:27017/?ssl=true&replicaSet=atlas-64o5c2-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const jobsCollection = client.db('taskSphere').collection('jobs');
+        const bidsCollection = client.db('taskSphere').collection('bids');
+
+        //Get all jobs data from db
+        app.get('/jobs', async (req, res) => {
+            const result = await jobsCollection.find().toArray();
+
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from taskSphere server.......');
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
