99a84a044fe3eb66f8bc878ed43de8c27e7541f0
Sultana Razia <sultanarazia483@gmail.com>
2025-02-04 18:26:59 +0600

    Get all jobs data from db for pagination

--- index.js
+++ index.js
@@ -190,6 +190,23 @@ async function run() {
             res.send(result);
         })
 
+        //Get all jobs data from db for pagination
+        app.get('/all-jobs', async (req, res) => {
+            const size = parseInt(req.query.size);
+            const page = parseInt(req.query.page) - 1;
+            console.log(size, page);
+            const result = await jobsCollection.find().skip(page * size).limit(size).toArray();
+
+            res.send(result);
+        })
+
+        //Get all jobs data count from db
+        app.get('/jobs-count', async (req, res) => {
+            const count = await jobsCollection.countDocuments();
+
+            res.send({ count });
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
